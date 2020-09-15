83f3f8491c5a83e3d13f2c7e7e3f42f5042d8033
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-09-14 22:08:30 -0500

    crud done

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,13 +1,50 @@
 class FlowersController < ApplicationController
+  before_action :current_flower, only: [:show, :edit, :update, :destroy]
+
   def index
+    @flowers = Flower.all
   end
 
   def show
+   
   end
 
   def new
+    @flower = Flower.new
+  end
+
+  def create
+    flower = Flower.create(flower_params)
+
+    redirect_to flowers_path(flower)
   end
 
   def edit
+ 
   end
+
+  def update
+    
+    @flower.update(flower_params)
+
+    redirect_to flower_path(@flower)
+  end
+
+  def destroy
+
+      @flower.destroy
+
+      redirect_to flowers_path
+    end 
+    
+    private
+
+    def flower_params
+      params.require(:flower).permit(:name, :color, :season)
+    end
+
+    def current_flower
+      @flower = Flower.find(params[:id])
+    end
+
 end
