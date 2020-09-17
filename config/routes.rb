ccf4d06837b0fefa4c5cc65e13fe4cef7154a6a3
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-09-17 13:45:03 -0500

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-
+  
+  get '/' =>  "home#index"
+  resources :users
+  # root 'home#index'
+  get '/flowers' => "flowers#index"
   resources :flowers
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_011811) do
+ActiveRecord::Schema.define(version: 2020_09_15_221851) do
 
   create_table "flowers", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_09_15_011811) do
     t.string "season"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "first_name"
+    t.text "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
