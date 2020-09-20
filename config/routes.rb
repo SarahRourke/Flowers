0eb53b7da223ab8b7ce9d1eaadc83cfd75709232
Sarah Rourke <sarahrourke@Sarahs-MBP.attlocal.net>
2020-09-20 16:15:44 -0500

    auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
 Rails.application.routes.draw do
-  
-  get '/' =>  "home#index"
-  resources :users
-  # root 'home#index'
-  get '/flowers' => "flowers#index"
   resources :flowers
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  get '/' =>  "home#index"
+  
+  # resources :users
+  root 'home#index'
+  get '/flowers' => "flowers#index"
+  
+  get 'signup', to: "users#new", as: "signup"
+  get '/login' => "sessions#new" 
+  get 'logout', to: "sessions#destroy", as: "logout"
+
+  get '/users/:id' => "users#show"
+  # get '/users/new' => "users#new"
+  post '/users' => "users#create"
+  
+
+  
+
   
 end
